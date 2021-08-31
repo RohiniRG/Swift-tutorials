b9e59fc95e926f801898d15f1ace396934a8c757
Rohini Rao <64858610+RohiniRG@users.noreply.github.com>
2021-08-31 20:42:43 +0530

    added arrays and dicts

---
+++ arraysDicts.swift
@@ -0,0 +1,28 @@
+// Arrays
+
+var list = ["Pam", "Jim", "Karen"]
+
+var numbers: [Int] = [1, 2, 3]
+
+var multi: [Any] = [1, "Dwight", 0.1, ""]
+
+for i in numbers {
+    print(i)
+}
+
+for i in 0...numbers.count-1 {
+    print(numbers[i])
+}
+
+// Dictionary
+var pets:[String: Int] = ["dogs": 3, "cats": 2, "cute hippos": 1]
+
+
+for i in pets {
+    print(i)
+}
+
+for (k, v) in pets {
+    print(v)
+}
+
